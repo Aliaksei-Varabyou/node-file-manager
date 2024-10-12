4ecabf34d7a2f245460e4ced051fe6e397de0ebc
Aliaksei Varabyou <aliaksei_varabyou@epam.com>
2024-10-13 01:16:26 +0200

    readable stream with prompt and exit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
 import { getUserName, parseArgs } from "./helpers/cli.js";
 
-const username = getUserName(parseArgs());
+// Greeting Block
+const args = parseArgs();
+const username = getUserName(args);
 console.log(`Welcome to the File Manager, ${username}!`);
+
+const rl = readline.createInterface({
+  input, output, prompt: `enter command: `
+});
+
+rl.prompt();
+
+rl.on('line', line => {
+  const command = line.trim();
+  if (command === '.exit' || command === 'exit') {
+    rl.close();
+  } else {
+    console.log('New command - ', command);
+    rl.prompt();
+  }
+}).on('close', () => {
+  console.log('Goodbye,', username);
+});
