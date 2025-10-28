b8e46d19bc7939e8db53c2ba6fdb9f44647f94db
Aliaksei-Varabyou <aleksey.vorobyov@gmail.com>
2025-10-28 21:55:03 +0100

    feat: homedir, rl.prompt

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import { createInterface } from 'node:readline/promises';
-import { stdin as input, stdout as output } from 'node:process';
-
+import { chdir, cwd, stdin as input, stdout as output } from 'node:process';
 import { getUserName } from "./utils/cli.js";
+import { setColor, writeMessage } from './utils/messages.js';
+import { homedir } from 'node:os';
 
 
 const userName = getUserName();
-console.log(`Welcome to the File Manager, ${userName}!`);
+writeMessage(`Welcome to the File Manager, ${userName}!`, 'green');
+chdir(homedir());
 
-const rl = createInterface({ input, output });
+const rl = createInterface({ input, output, 
+  prompt: `${setColor(`You are currently in <<${cwd()}>>`, 36)}\n${setColor('Enter command', 33)} > `
+});
+
+rl.prompt();
 
 rl.on('line', line => {
   console.log(`Received: ${line}`);
+  rl.prompt();
 });
+
 rl.on('close', () => {
-  console.log(`\n--------\nThank you for using File Manager, ${userName}, goodbye!\n--------`);
+  writeMessage(`Thank you for using File Manager, ${userName}, goodbye!`, 'green');
 })
---
+++ src/utils/messages.js
@@ -0,0 +1,12 @@
+export const setColor = (text, code) => `\x1b[${code}m${text}\x1b[0m`;
+const colors = {
+  'red': 31,
+  'green': 32,
+  'yellow': 33,
+  'blue': 34,
+  'white': 37
+}
+
+export const writeMessage = (message, color = 'white') => {
+  console.log(setColor(`\n--------\n${message}\n--------`, colors[color] || 37));
+}
