412d806d50f7a5933e75f0705d086ec93c0fd86b
Aliaksei-Varabyou <aleksey.vorobyov@gmail.com>
2025-10-28 21:02:15 +0100

    feat: invitation and exit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
+import { createInterface } from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
 
-console.log('Welcome to the File Manager');
+import { getUserName } from "./utils/cli.js";
+
+
+const userName = getUserName();
+console.log(`Welcome to the File Manager, ${userName}!`);
+
+const rl = createInterface({ input, output });
+
+rl.on('line', line => {
+  console.log(`Received: ${line}`);
+});
+rl.on('close', () => {
+  console.log(`\n--------\nThank you for using File Manager, ${userName}, goodbye!\n--------`);
+})
---
+++ src/utils/cli.js
@@ -0,0 +1,20 @@
+const USER = 'username';
+
+const parseArgs = () => {
+  const args = [];
+  process.argv.slice(2)
+  .filter(arg => arg.startsWith('--'))
+  .forEach(arg => {
+    const argData = arg.split('=');
+    args[argData[0].slice(2)] = argData[1];
+  });
+  return args;
+}
+
+export const getUserName = () => {
+  const args = parseArgs();
+  if (args[USER] === undefined) {
+    return 'Guest'
+  }
+  return args[USER];
+};
